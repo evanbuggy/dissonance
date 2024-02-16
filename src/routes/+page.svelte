15bace6b0278ad8701fbf94d556a2fd41fcc6d6f
evanbuggy <22348069@studentmail.ul.ie>
2024-02-16 16:04:27 +0000

    Lots of Stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome!</h1>
+<p>Welcome to my website! Ran and owned by me, Buggy. This site will be home to my many ramblings of gaming,
+    computer science stuff, art, and whatever other stuff I still have on my hard drive. We hope you enjoy your 
+    stay...
+</p>
+
+<style>
+    h1 {
+        color: #403d3d;
+        font-family: "ACNH";
+        line-height: 45px;
+        text-align: center;
+    }
+
+    p {
+        color: #403d3d;
+        font-family: "ACNH";
+        line-height: 28px;
+    }
+
+    @font-face {
+        font-family: ACNH;
+        src: url("$lib/fonts/FOT-Seurat_Pro_B.otf") format("opentype");
+    }
+</style>
